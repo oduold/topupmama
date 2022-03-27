e5d8f72dc3caf391641ef1de17980c66222b9f7a
Donald O <donopiyo@gmail.com>
2022-03-27 05:14:26 +0300

    mutate ip value from db

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
+use function Illuminate\Database\Eloquent\Casts\get;
 
 class Comment extends Model {
     
@@ -23,6 +25,14 @@ class Comment extends Model {
      * @var array
      */
     protected $hidden = ['book_id'];
+    
+    /**
+     * 
+     * @return Attribute
+     */   
+    protected function getIpAttribute($value) {
+        return long2ip($value);
+    }
 
     public function book() : BelongsTo {
         return $this->belongsTo(Book::class,'book_id');
